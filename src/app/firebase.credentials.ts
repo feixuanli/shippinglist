eafa792f54de4a50c2a63773d08fec1a59d4d83d
Feixuan Li <fli@swensonhe.com>
2018-01-05 15:27:01 -0800

    final

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAu7U-Lx6s-jj8L__HOvqrHLDhwO5BkX0Q",
+    authDomain: "shoppinglist-139ce.firebaseapp.com",
+    databaseURL: "https://shoppinglist-139ce.firebaseio.com",
+    projectId: "shoppinglist-139ce",
+    storageBucket: "shoppinglist-139ce.appspot.com",
+    messagingSenderId: "369318376671"
+}
---
+++ src/services/shopping-list/shopping-list.service.ts
@@ -0,0 +1,21 @@
+import { AngularFireDatabase } from "angularfire2/database";
+import { Injectable } from "@angular/core";
+import { Item } from '../../model/item/item.module';
+
+@Injectable()
+export class ShoppingListService {
+    private shoppingListRef = this.db.list<Item>('shopping-list');
+    constructor(private db: AngularFireDatabase){}    
+    getShoppingList(){
+        return this.shoppingListRef;
+    }
+    addItem(item: Item) {
+        return this.shoppingListRef.push(item);
+    }
+    editItem(item: Item){
+        return this.shoppingListRef.update(item.key, item);
+    }
+    removeItem(item: Item){
+        return this.shoppingListRef.remove(item.key);
+    }
+}
